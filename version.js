ce70a80f6750afb30b885060c9ebe4a717d5f9f6
inglele <inglele@users.noreply.github.com>
2026-07-03 21:47:47 +0200

    deploy v1.3-31-gc60c136

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 // GENERATO da deploy.sh — non modificare a mano. Versione da git describe.
-window.APP_VERSION = 'v1.3-29-g376eda4';
+window.APP_VERSION = 'v1.3-31-gc60c136';
 window.APP_BUILD_DATE = '2026-07-03';
